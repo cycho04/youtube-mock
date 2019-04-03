d395bca2803b73dd8a8a507f25330eb0934a3371
Chris Y Cho <chrisycho04@gmail.com>
2019-04-03 03:26:13 -0700

    updating serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { unregister } from './registerServiceWorker';
 
 
 import App from './components/App';
@@ -7,3 +8,4 @@ import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+unregister()
