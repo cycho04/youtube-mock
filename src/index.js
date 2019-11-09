f1d7c0b9abd3abd3f515b3d925c2f2cd80b68127
Chris Cho <chrisycho04@gmail.com>
2019-11-08 22:15:29 -0800

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { unregister } from './registerServiceWorker';
-
+import reducers from './redcuers';
 
 import App from './components/App';
 
+//redux, redux-thunk, and dev tools set up
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, enhancer);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 unregister()
